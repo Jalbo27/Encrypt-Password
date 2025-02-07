9dc5316546ccc82d689fb3b49c4c53ff14394606
Jalbo27 <alberto.lorenzini2004@gmail.com>
2025-02-07 15:56:31 +0100

    fixed console log homepage.js

--- encryptimages/WebSite/static/homepage.js
+++ encryptimages/WebSite/static/homepage.js
@@ -1,30 +1,31 @@
-var container = [{}];
-var index = 0;
+let container = [];
+let index = 0;
 
 window.onload = () => {
   let submit_btn = document.getElementById("liveToastBtn");
   submit_btn.addEventListener("click", function (e) {
-  let name_pass = document.getElementsByName("name-control").value;
-  let username = document.getElementsByName("username-control")[0].value;
-  let password = document.getElementsByName("password-control")[0].value;
-  let uri = document.getElementsByName("uri-control")[0].value;
-  container[index]['name'] = name_pass;
-  container[index]['username'] = username;
-  container[index]['password'] = password;
-  container[index]['uri'] = uri;
-  console.log(name_pass);
-  console.log(username);
-  console.log(password);
-  console.log(uri);
-  alert(toString(name_pass) + ' ' + toString(username) + ' ' + toString(password) + ' ' + toString(uri));
-  // container.forEach((element) => {
-  //     console.log(typeof (element))
-  //     if (element === String && element != "")
-  //         return true;
-  //     else {
-  //         alert('Dei campi sono vuoti oppure sono errati');
-  //         return false;
-  //     }
-  // })         
-});
+    /*const name_pass = */ console.log(document.getElementsByName("name-control")[0].value);
+    const username = document.getElementsByName("username-control")[0].value;
+    const password = document.getElementsByName("password-control")[0].value;
+    const uri = document.getElementsByName("uri-control")[0].value;
+    container.push(
+      {
+        //'name': name_pass, 
+        'username': username,
+        'password': password,
+        'uri': uri
+      });
+    //console.log(name_pass)
+    console.log('name: ' + container[index].name);
+    console.log('username: ' + container[index].username);
+    console.log('password: ' + container[index].password);
+    console.log('uri: ' + container[index].uri);
+    console.log(container);
+  
+  // container.forEach((object1, index, line) => {
+  //   object1.forEach((internal_object, index2, line2) => {
+  //     console.log(line[index][index2])
+  //   });
+  // });
+  });
 }
