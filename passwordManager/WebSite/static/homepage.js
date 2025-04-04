9e4b4a4905c5f2118b7d9fd5bbbb36a202e72b4d
Jalbo27 <alberto.lorenzini2004@gmail.com>
2025-04-04 16:36:55 +0200

    deleting all unused files

--- passwordManager/WebSite/static/homepage.js
+++ passwordManager/WebSite/static/homepage.js
@@ -3,6 +3,16 @@ let url = window.location.href;
 let url_login = window.location.origin + '/login';
 
 window.onload = () => {
+  $(".toggle-password").click(function() {
+
+    $(this).toggleClass("fa-eye fa-eye-slash");
+    var input = $($(this).attr("toggle"));
+    if (input.attr("type") == "password") {
+      input.attr("type", "text");
+    } else {
+      input.attr("type", "password");
+    }
+  });
   /**
    * CONTROLLO DELLA LOGIN ESEGUITA DALL'UTENTE
    */
